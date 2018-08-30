7f696dbdc23b218cf88f6c538c63582d9e421975
Tudor Ilisoi <tudorilisoi@yahoo.com>
2018-08-30 16:19:12 +0300

    validation

--- src/App.js
+++ src/App.js
@@ -13,32 +13,62 @@ function getRandomNumber() {
 class App extends Component {
   constructor(props) {
     super(props)
-    
+
     this.state = {
       lastGeneratedNumber: getRandomNumber(),
-      userProvidedNumber: ''
+      userProvidedNumber: '',
+      isValid: null,
+      isGuessed: false,
+      message: '',
     }
   }
-  
-  handleInput(event){
+
+  handleInput(event) {
+    const value = event.target.value
+    const numValue = parseInt(value, 10);
+    let isValid = !isNaN(value)
+    if (numValue < min || numValue > max) {
+      isValid = false
+    }
+
     this.setState({
-      userProvidedNumber: event.target.value
+      userProvidedNumber: value,
+      isValid,
     })
   }
-  
+
+  handleGuess(event) {
+    event.preventDefault()
+    const { lastGeneratedNumber, userProvidedNumber } = this.state
+    if ('' + lastGeneratedNumber === userProvidedNumber) {
+      this.setState({ isGuessed: true, message: 'Lucky bastard!' })
+    } else {
+      //TODO generate another, start once over
+      this.setState({ message: 'Not by far!' })
+    }
+  }
+
   render() {
 
+    const { isValid, message, isGuessed } = this.state
+
     const buttonProps = {
-      onClick: this.handleInput.bind(this),
+      onClick: this.handleGuess.bind(this),
+      disabled: !isValid,
     }
 
+    const validationStr = isValid === null ? null : (isValid ? null : `Please enter a number between ${min} and ${max}`)
+
+    const startOverBtn = isGuessed ? (<button>Start over</button>) : null
 
     return (
-      
+
       <div className="App">
-        <input value={this.state.userProvidedNumber} onChange={this.handleInput.bind(this)}/>
-        <Button />
-        <pre>{JSON.stringify(this.state,2,2)}</pre>
+        <p>{message} {startOverBtn}</p>
+        <p>{validationStr}</p>
+        <input value={this.state.userProvidedNumber} onChange={this.handleInput.bind(this)} />
+        <Button {...buttonProps} />
+        <pre>{JSON.stringify(this.state, 2, 2)}</pre>
       </div>
     );
   }
